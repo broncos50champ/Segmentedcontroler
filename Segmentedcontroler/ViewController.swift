a08f9c1a50d51104cc07b902ec0ab9a0c8e56cda
Luis Segovia <luiissegovia@icloud.com>
2022-02-24 14:06:39 -0700

    mvp

--- Segmentedcontroler/ViewController.swift
+++ Segmentedcontroler/ViewController.swift
@@ -14,6 +14,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBOutlet weak var segOut: UISegmentedControl!
+    
+    
+    
+    @IBAction func lenguageAction(_ sender: Any)
+    {
+        switch segOut.selectedSegmentIndex
+        {
+        case 0:
+            helloLabel.text = "hello"
+        case 1:
+            helloLabel.text = "hola"
+        case 2:
+            helloLabel.text = "こんにちは"
+        default:
+            helloLabel.text = "hello"
+        }
+        
+        
+        
+    
+    
+    
+    
+        
+        
+        
+        
+        
+    }
+    
+    
 }
 
